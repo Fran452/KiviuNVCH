ed4fe478e89bdab28b70b7bbdcee0286173f718a
francisco1106 <flemaestevez@frba.utn.edu.ar>
2024-07-19 17:37:10 -0300

    eliminacion de los req.session para pasar a req.body

--- src/apis/controllerHomeApis.js
+++ src/apis/controllerHomeApis.js
@@ -27,7 +27,7 @@ const controlador = {
         try{
             let area = await dataBaseSQL.areas.findAll();
             area = area.map(area => {return {id_area: area.id_area,nombre_del_Area: area.nombre_del_Area}});
-            let api = {status: 0, codeError:"", objeto:{areas:area,usuario:req.session.user} };
+            let api = {status: 0, codeError:"", objeto:{areas:area,usuario:req.body.user} };
             res.json(api);
         }
         catch(error){
@@ -40,17 +40,18 @@ const controlador = {
         try{
             let area
             let BiArea
-            if(req.session.user.area == req.params.area){
+            if(req.body.user.area == req.body.area){
                 area = await dataBaseSQL.areas.findByPk(req.params.area);
                 BiArea = area.power_Bi;
-                res.json( {status: 0, codeError:"", objeto: BiArea })
-                return {status: 0, codeError:"", objeto: BiArea };
+                nombreArea=area.nombre_del_Area
+                res.json( {status: 0, codeError:"", objeto: {BiArea,nombreArea} })
+                return {status: 0, codeError:"", objeto: {BiArea,nombreArea} };
             }else{
-                if(req.session.user.puesto == 0 || req.session.user.puesto == 1){
+                if(req.body.user.puesto == 0 || req.body.user.puesto == 1){
                     area = await dataBaseSQL.areas.findByPk(req.params.area);
                     BiArea = area.power_Bi;
-                    res.json({status: 0, codeError:"", objeto: BiArea })
-                    return {status: 0, codeError:"", objeto: BiArea };
+                    res.json({status: 0, codeError:"", objeto: {BiArea,nombreArea} })
+                    return {status: 0, codeError:"", objeto: {BiArea,nombreArea} };
                 }else{
                     res.json({status: 99, codeError:"No tiene permisos", objeto: "" })
                     return {status: 99, codeError:"No tiene permisos", objeto: "" };
