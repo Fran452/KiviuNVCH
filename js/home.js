d8e24ff35bebadebaf6a95224614953ceaa99b66
Fran452 <franciscolemacr@gmail.com>
2024-07-23 12:48:58 -0300

    modificacion view de tareas pasando el area, el empleado y el area de apoyo

---
+++ js/home.js
@@ -0,0 +1,13 @@
+fetch('http://localhost:3030/apis/plan-accion',{
+    method:'POST',
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({user:{id: 4,nombre: 'Francisco Lema',area: 1,puesto: 2,mail: 'franciscolemacr@gmail.com'}})
+})
+.then(api => {
+    return api.json();
+})
+.then(api => {
+    console.log(api)
+})
